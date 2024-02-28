4dc3c9b3ee96a582e386bc8c7e2b37d054d44a31
MartynasPupkus <pupkus.martynas@gmail.com>
2024-02-28 17:11:16 +0200

    bandau pridet komentaru sekcija

--- blog_martynas/blog/models.py
+++ blog_martynas/blog/models.py
@@ -4,6 +4,7 @@ from django.utils import timezone
 from django.utils.translation import gettext as _
 from django.urls import reverse
 
+
 class Post(models.Model):
 
     class NewManager(models.Manager):
@@ -34,3 +35,21 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Comment(models.Model):
+
+    post = models.ForeignKey(Post,
+                             on_delete=models.CASCADE,
+                             related_name='comments')
+    name = models.CharField(max_length=50)
+    email = models.EmailField()
+    content = models.TextField()
+    publish = models.DateTimeField(auto_now_add=True)
+    status = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ('publish',)
+
+    def __str__(self):
+        return f'Comment by {self.name}'
--- blog_martynas/blog/views.py
+++ blog_martynas/blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpRequest, HttpResponse
+from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
 from .models import Post
+from .forms import NewCommentForm
 
 def home(request: HttpRequest) -> HttpResponse:
     all_posts = Post.newmanager.all()
@@ -8,4 +9,26 @@ def home(request: HttpRequest) -> HttpResponse:
 
 def post_single(request, post):
     post = get_object_or_404(Post, slug=post, status='published')
-    return render(request, 'single.html', {'post': post})
+    comments = post.comments.filter(status=True)
+    user_comment = None
+
+    if request.method == 'POST':
+        comment_form = NewCommentForm(request.POST)
+        if comment_form.is_valid():
+            user_comment = comment_form.save(commit=False)
+            user_comment.post = post
+            user_comment.save()
+            return HttpResponseRedirect('/' + post.slug)
+    else:
+        comment_form = NewCommentForm()
+    return render(
+        request, 
+        'single.html', 
+        {
+            'post': post,
+            'comments': user_comment, 
+            'comments': comments, 
+            'comment_form': comment_form 
+        },
+    )
+    
